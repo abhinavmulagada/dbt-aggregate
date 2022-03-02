a174f75253ffdd82db3d4f25e168c16464a5d1b4
Abhinav Mulagada <abhinav@abhinavs-mbp-2.lan>
2022-03-02 14:13:13 -0800

    incremental model to update most recent row in table instead of constantly appending

--- models/aggregate-transactions.sql
+++ models/aggregate-transactions.sql
@@ -20,17 +20,17 @@ SUM(CAST(t.value as BIGNUMERIC)) as total_value,
 MIN(CAST(t.value as BIGNUMERIC)) as min_value, 
 MAX(CAST(t.value as BIGNUMERIC)) as max_value, 
 CAST(AVG(CAST(t.value as BIGNUMERIC)) as BIGNUMERIC) as avg_value, 
-DATE(b.timestamp) as `date`
+DATE(b.timestamp) as date,
+MAX(t.updated_at) as updated_at
 FROM `celo-testnet-production.blockscout_data.rpl_transactions` t 
 LEFT JOIN `celo-testnet-production.blockscout_data.rpl_blocks` b 
-ON t.block_hash = b.hash 
-WHERE DATE(b.timestamp) >= DATE_ADD(CURRENT_DATE(), INTERVAL -1 DAY) 
-AND DATE(b.timestamp) < CURRENT_DATE() 
-GROUP BY `date`
+ON t.block_hash = b.hash
+--WHERE DATE(b.timestamp) >= DATE_ADD(CURRENT_DATE(), INTERVAL -1 DAY) 
+--AND DATE(b.timestamp) < CURRENT_DATE() 
 
 {% if is_incremental() %}
-
-  -- this filter will only be applied on an incremental run
-  where date >= (select max(date) from {{ this }})
-
+  where t.updated_at >= (select max(t.updated_at) from {{ this }} 
+  WHERE DATE(b.timestamp) >= DATE_ADD(CURRENT_DATE(), INTERVAL -1 DAY) AND DATE(b.timestamp) < CURRENT_DATE() )
 {% endif %}
+
+GROUP BY `date`
