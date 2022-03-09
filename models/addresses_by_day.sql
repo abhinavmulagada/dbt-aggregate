93e41d762ff300ab3d970e8fe40861f342d01943
Abhinav Mulagada <abhinav@abhinavs-mbp-2.lan>
2022-03-09 14:45:15 -0800

    creating model for addresses_by_day

---
+++ models/addresses_by_day.sql
@@ -0,0 +1,18 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='date'
+    )
+}}
+
+SELECT SUM(CASE when CONTAINS_SUBSTR((is_contract), 'true') then 1 else 0 end) as contract_addresses, 
+       SUM(CASE when CONTAINS_SUBSTR((is_contract), 'false') then 1 else 0 end) as wallet_addresses, 
+       DATE(inserted_at) as `date` 
+FROM `celo-testnet-production.blockscout_data.rpl_addresses` 
+
+{% if is_incremental() %}
+  WHERE DATE(`inserted_at`) > (SELECT MAX(`date`) as max_date 
+                            FROM `celo-testnet-production.blockscout_data.addresses_by_day`)
+{% endif %}
+
+GROUP BY DATE(`inserted_at`)
