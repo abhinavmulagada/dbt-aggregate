1054223f56d1edae4ecf821554b020e815163f2c
Abhinav Mulagada <abhinav@abhinavs-mbp-2.lan>
2022-03-17 15:10:59 -0700

    changing format

--- models/aggregate_active_addresses_by_day.sql
+++ models/aggregate_active_addresses_by_day.sql
@@ -5,22 +5,30 @@
     )
 }}
 
-select DATE (t.inserted_at) as `date`,
-       count(case when CONTAINS_SUBSTR((a.is_contract), 'true') 
-             and DATE(t.inserted_at) >= DATE_ADD(CURRENT_DATE(), INTERVAL -30 DAY) then 1 end) as returning_contract_address,
-       count(case when CONTAINS_SUBSTR((a.is_contract), 'false') 
-             and DATE(t.inserted_at) >= DATE_ADD(CURRENT_DATE(), INTERVAL -30 DAY) then 1 end) as returning_wallet_address,
-       count(case when CONTAINS_SUBSTR((a.is_contract), 'true') 
-             and DATE(t.inserted_at) < DATE_ADD(CURRENT_DATE(), INTERVAL -30 DAY) then 1 end) as new_contract_address,
-       count(case when CONTAINS_SUBSTR((a.is_contract), 'false') 
-             and DATE(t.inserted_at) < DATE_ADD(CURRENT_DATE(), INTERVAL -30 DAY) then 1 end) as new_wallet_address,
-from `celo-testnet-production.blockscout_data.rpl_transactions` t
-inner join `celo-testnet-production.blockscout_data.rpl_addresses` a on a.`hash` = t.from_address_hash
+with transaction_data as(
+      select DATE (t.inserted_at) as `date`,
+            a.is_contract,
+            CASE WHEN DATE(t.inserted_at) < DATE_ADD(CURRENT_DATE(), INTERVAL -30 DAY) 
+                 then 'new' else 'returning' end as address_status
+      
+      from `celo-testnet-production.blockscout_data.rpl_transactions` t
+      inner join `celo-testnet-production.blockscout_data.rpl_addresses` a on a.`hash` = t.from_address_hash
+),
+transaction_data_2 as (
+      select `date`,
+            CASE WHEN is_contract and address_status = 'new' then 'new_contract_address' 
+            WHEN is_contract and address_status = 'returning' then 'returning_contract_address'
+            WHEN NOT is_contract and address_status = 'new' then 'new_wallet_address'
+            WHEN NOT is_contract and address_status = 'returning' then 'returning_wallet_address' end as type,
+      from transaction_data
+)
+select count(*) as count, type, date
+from transaction_data_2 
 
 
 {% if is_incremental() %}
-  WHERE (t.inserted_at) > (SELECT MAX(inserted_at) as max_date 
+  WHERE `date` > (SELECT MAX(inserted_at) as max_date 
                             FROM `celo-testnet-production.blockscout_data.rpl_transactions`)
 {% endif %}
 
-group by `date`
+group by type, date
