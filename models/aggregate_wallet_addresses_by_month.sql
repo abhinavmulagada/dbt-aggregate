3e2ebbac279bab8fa54b377916cec5cde395f8e7
Abhinav <97618969+abhinavmulagada@users.noreply.github.com>
2022-03-24 12:55:48 -0700

    Update aggregate_wallet_addresses_by_month.sql

--- models/aggregate_wallet_addresses_by_month.sql
+++ models/aggregate_wallet_addresses_by_month.sql
@@ -1,7 +1,7 @@
 {{
     config(
         materialized='incremental',
-        unique_key='date'
+        unique_key='month'
     )
 }}
 
@@ -10,8 +10,9 @@ select sum(new_wallet_addresses) as new_wallet_addresses_by_month,
        DATE_TRUNC(date, MONTH) as month,
        MAX(date) as most_recent_date
 from {{ref('aggregate_wallet_addresses_by_day')}}
+
 {% if is_incremental() %}
-  WHERE `most_recent_date` < (SELECT MAX(date) as max_date 
-                            FROM {{ref('aggregate_wallet_addresses_by_day')}})
+  WHERE `most_recent_date` > (SELECT MAX(date) as max_date FROM {{ this }})
 {% endif %}
+
 group by month
