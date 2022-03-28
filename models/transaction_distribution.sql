c08dd032adf67f44b69c462dae5615623e2fd618
Abhinav Mulagada <abhinav@abhinavs-mbp-2.lan>
2022-03-28 15:54:45 -0700

    adding name logic

--- models/transaction_distribution.sql
+++ models/transaction_distribution.sql
@@ -1,12 +1,5 @@
-{{
-    config(
-        materialized='incremental',
-        unique_key='date'
-    )
-}}
-
 with daily_data as (
-select to_address_hash, 
+select distinct to_address_hash, 
        date(b.timestamp) as date,
        count(*) as daily_total
    from `celo-testnet-production.blockscout_data.rpl_transactions` t 
@@ -15,32 +8,33 @@ select to_address_hash,
    group by t.to_address_hash, date
 ),
 total_data as (
-    select sum(daily_total) as total, 
+    select distinct sum(daily_total) as total, 
        to_address_hash
     from daily_data 
     group by to_address_hash
 ),
 weekly_data as (
-    select sum(daily_total) as weekly_total,   
+    select distinct sum(daily_total) as weekly_total,   
            to_address_hash,
            max(date) as max_date
     from daily_data 
     where date < DATE(CURRENT_TIMESTAMP())
     and date >= DATE_SUB(DATE(CURRENT_TIMESTAMP()), INTERVAL 7 DAY)
     group by to_address_hash
+),
+address_name_data as(
+    select * from 
+    EXTERNAL_QUERY(
+    'us-west1.blockscout-rc13',
+    '''SELECT CONCAT('0x', ENCODE(address_hash, 'hex')) as address_hash,
+              name from address_names''',
+              '{"default_type_for_decimal_columns":"float64"}')
 )
 select t.total, 
        w.weekly_total, 
-       w.to_address_hash, 
-       w.max_date 
+       w.to_address_hash,
+       case when a.name is not null then a.name else 'N/A' end as name
 from weekly_data w 
-inner join total_data t 
-on t.to_address_hash = w.to_address_hash 
-
-
-{% if is_incremental() %}
-  where `max_date` >= (select max(`date`) from {{ this }})
-{% endif %}
-
-ORDER BY
-  t.total DESC
+inner join total_data t on t.to_address_hash = w.to_address_hash 
+left join address_name_data a on t.to_address_hash = a.address_hash
+order by total desc
