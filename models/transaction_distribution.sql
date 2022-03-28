c34034e0599bcc8e1d5cc757576d06a0b9445a36
Abhinav <97618969+abhinavmulagada@users.noreply.github.com>
2022-03-28 13:51:00 -0700

    Update transaction_distribution.sql

--- models/transaction_distribution.sql
+++ models/transaction_distribution.sql
@@ -6,7 +6,7 @@
 }}
 
 with daily_data as (
-select to_address_hash, 
+select distinct to_address_hash, 
        date(b.timestamp) as date,
        count(*) as daily_total
    from `celo-testnet-production.blockscout_data.rpl_transactions` t 
@@ -15,13 +15,13 @@ select to_address_hash,
    group by t.to_address_hash, date
 ),
 total_data as (
-    select sum(daily_total) as total, 
+    select distinct sum(daily_total) as total, 
        to_address_hash
     from daily_data 
     group by to_address_hash
 ),
 weekly_data as (
-    select sum(daily_total) as weekly_total,   
+    select distinct sum(daily_total) as weekly_total,   
            to_address_hash,
            max(date) as max_date
     from daily_data 
