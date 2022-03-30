f113328ddbfe1bf16914fc333cf8fdf1ae0046d5
Abhinav Mulagada <abhinav@abhinavs-mbp-2.lan>
2022-03-29 19:46:04 -0700

    adding token transfer to aggregation

--- models/aggregate_wallet_addresses_by_day.sql
+++ models/aggregate_wallet_addresses_by_day.sql
@@ -7,9 +7,20 @@
 
 with transaction_count as(
     select distinct
-       count(*) over (partition by from_address_hash) as number_of_transactions, 
+       count(*) over (partition by from_address_hash) as number_of_wallet_transactions, 
        from_address_hash,
     from `celo-testnet-production.blockscout_data.rpl_transactions` 
+),
+token_transfer_count as(
+    select distinct
+       count(*) over (partition by from_address_hash) as number_of_token_transactions, 
+       from_address_hash,
+    from `celo-testnet-production.blockscout_data.rpl_token_transfers` 
+),
+total_transfer_count as (
+    select from_address_hash, number_of_wallet_transactions as number_of_transactions from transaction_count
+    union all
+    select from_address_hash, number_of_token_transactions as number_of_transactions from token_transfer_count
 ),
 wallet_by_day as (
  select  DATE(b.timestamp)  as `date`, 
@@ -22,7 +33,7 @@ wallet_by_day as (
 select w.`date`,
 count(case when t.number_of_transactions < 2 then 1 end) as new_wallet_addresses,
 count(case when t.number_of_transactions > 1 then 1 end) as returning_wallet_addresses
-from wallet_by_day w left join transaction_count t 
+from wallet_by_day w left join total_transfer_count t 
 on w.from_address_hash = t.from_address_hash
 
 
