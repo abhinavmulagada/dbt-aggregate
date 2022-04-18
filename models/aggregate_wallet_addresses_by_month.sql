a3e6fdd2692dd6628d3b861ab30ba3875b57a262
Abhinav Mulagada <abhinav@abhinavs-mbp-2.lan>
2022-04-18 12:43:36 -0700

    fixing syntax

--- models/aggregate_wallet_addresses_by_month.sql
+++ models/aggregate_wallet_addresses_by_month.sql
@@ -5,15 +5,18 @@
     )
 }}
 
-select sum(new_wallet_addresses) as new_wallet_addresses_by_month,
+with address_data as (
+  select sum(new_wallet_addresses) as new_wallet_addresses_by_month,
        sum(returning_wallet_addresses) as returning_wallet_addresses_by_month,
        DATE_TRUNC(date, MONTH) as month,
        MAX(date) as most_recent_date
-from {{ref('aggregate_wallet_addresses_by_day')}}
-where `date` is not null
+  from {{ref('aggregate_wallet_addresses_by_day')}}
+  where `date` is not null
+  group by month
+)
+select * from address_data
+
 
 {% if is_incremental() %}
   WHERE `most_recent_date` > (SELECT MAX(`most_recent_date`) as max_date FROM {{ this }})
 {% endif %}
-
-group by month
